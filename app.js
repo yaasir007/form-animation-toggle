6786aa89b5edd15c4ee5f996822b4bf122044811
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-12-18 01:42:26 +0400

    add animation and js

---
+++ app.js
@@ -0,0 +1,13 @@
+const allRadios = document.querySelectorAll('input[type="radio"]');
+const title = document.querySelector("#title");
+const fullPara = document.querySelector("#fullPara");
+const para = document.querySelector(".para");
+
+
+allRadios.forEach((element) => {
+  element.addEventListener('click', () => {
+    para.classList.remove("none");
+    title.innerHTML = element.value;
+
+  })
+});
