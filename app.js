df9cc7dfb937848ffe44a3911ffbd2f8285ff878
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-12-21 00:13:13 +0400

    completed;

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ const title = document.querySelector("#title");
 const fullPara = document.querySelector("#fullPara");
 const para = document.querySelector(".para");
 
-
-
 allRadios.forEach((element) => {
   element.addEventListener('click', () => {
-    para.classList.remove("none");
+    para.classList.toggle("none");
     title.innerHTML = element.value;
-    if (element.value === "drupal") {
-      fullPara.innerText = 'Hello';
-    }
-
-    if (element.value === "umbraco") {
-      fullPara.innerText = "first";
-    }
   })
 });
